6ba462850e2c56e46cb96422c29e302a3874e4e3
Matthijs Booman <matthijs.booman@hotmail.com>
2018-11-01 21:16:34 +0100

    added circle management

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import time # temp
 
 def init_canvas():
   canvas = Canvas(game, width=width, height=height, background="#fff")
@@ -8,17 +7,32 @@ def init_canvas():
   canvas.create_rectangle(draw_offset, draw_offset, width - draw_offset, height - draw_offset, fill="#4259f4")
   return canvas
 
-def draw_cirlce():
-  offset = draw_offset * 2
-  return canvas.create_oval(offset, offset, offset + 200, offset + 200, fill="#fff")
+def draw_circle(posX, posY, size):
+  return canvas.create_oval(posX, posY, posX + size, posY + size, fill="#fff")
 
-def draw_grid():
-  pass
+def draw_grid(width, height):
+  columns = []
+
+  for column in range(gridColumns):
+    rows = []
+
+    for row in range(gridRows):
+      # add id by creating oval
+      #circle_id = draw_circle()
+      rows.insert(row, {"state": ""})
+    
+    columns.insert(column, tuple(rows))
+
+  return tuple(columns)
 
 def test(event):
   item = event.widget.find_closest(event.x, event.y)[0]
   canvas.itemconfig(item, fill="#F00")
   # canvas.delete(item)
+
+grid = ()
+gridColumns = 7
+gridRows = 6
 
 game = Tk()
 game.configure(background='white')
@@ -27,13 +41,12 @@ game.title("Connect 4 by Matthijs Booman")
 width = 600
 height = 600
 draw_offset = 5
+circle_size = 60
 
 canvas = init_canvas()
-# draw_grid()
-oval = draw_cirlce()
-canvas.tag_bind(oval, '<Button-1>', test)
+grid = draw_grid()
+print(grid)
+#oval = draw_circle()
+#canvas.tag_bind(oval, '<Button-1>', test)
 
-game.mainloop()
-
-
-
+#game.mainloop()
